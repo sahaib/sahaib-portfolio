eb09b6e0cc01465401a847a63900ff8c03173eeb
sahaib <sahaibsingh001@live.com>
2025-12-04 05:09:47 +0530

    updated resume link and og-image

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
   pageType: 'website',
-  author: "Louai Boumediene",
-  profile: "https://louai.site",
-  desc: "My digital space, projects, insights, and thoughts on software engineering.",
-  title: "Louai Boumediene",
-  ogImage: '/og-image.jpg',
+  author: "Sahaib Singh",
+  profile: "https://work.sahaibsingh.com",
+  desc: "My digital space, projects, insights, and thoughts on product & technical solutions.",
+  title: "Sahaib Singh",
+  ogImage: '/og-images/sahaib-singh.png',
   postPerPage: 6,
 } as const;
